e0174a433e5b42bdfe330dce86f6768a2773d499
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-06-10 14:52:21 +1000

    docs: update build file

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,12 @@
+# Ensure the script exits on error
+set -e
+
+# Install pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+
+# Install virtualenv
+pip3 install virtualenv
+virtualenv venv
 pip3 install -r requirements.txt 
 python3.9 src/manage.py collectstatic
