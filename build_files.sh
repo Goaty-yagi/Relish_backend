7ab590aa645e16dbdab981ae17d8b22debc7e099
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-06-10 15:26:58 +1000

    docs: update requirements.txt and build file

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,2 @@
-export PYTHONPATH=$(pwd)/src:$PYTHONPATH
-echo "PYTHONPATH is set to: $PYTHONPATH"
-pip3 install -r requirements.txt 
+pip3 install -r src/requirements.txt 
 python3.9 src/manage.py collectstatic
