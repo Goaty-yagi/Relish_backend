da82837fffcf6376f05c78ef3e763a259ab2b2b6
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-06-10 15:19:08 +1000

    docs: update build file and settings

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
+export PYTHONPATH=$(pwd)/src:$PYTHONPATH
+echo "PYTHONPATH is set to: $PYTHONPATH"
 pip3 install -r requirements.txt 
 python3.9 src/manage.py collectstatic
--- src/root/settings.py
+++ src/root/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 print("OS:", platform.system())
 
 dotenv_file = BASE_DIR / 'env.local'
