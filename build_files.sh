86981f800937ab441da737e76984b7289a763f27
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-06-10 14:41:29 +1000

    docs: update build file

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python get-pip.py
 
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
